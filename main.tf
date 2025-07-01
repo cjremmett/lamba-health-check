2f3e7dbfffb9e5a7005a76a3022a985f97f70a0b
cjremmett <cjremmett@gmail.com>
2025-07-01 08:18:33 -0400

    add terraform file for deployment to aws

---
+++ main.tf
@@ -0,0 +1,113 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "archive_file" "zip_index_file" {
+  type = "zip"
+  source_file = "index.mjs"
+  output_path = "function.zip"
+}
+
+resource "aws_iam_role" "lambda_execution_role" {
+  name = "lambda_execution_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action    = "sts:AssumeRole",
+        Effect    = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "lambda_policy" {
+  name        = "lambda_policy"
+  description = "Policy to allow Lambda to access DynamoDB and make API calls"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = [
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+          "dynamodb:Scan"
+        ],
+        Effect   = "Allow",
+        Resource = "*"
+      },
+      {
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Effect   = "Allow",
+        Resource = "*"
+      },
+      {
+        Action = "ec2:CreateNetworkInterface",
+        Effect = "Allow",
+        Resource = "*"
+      },
+      {
+        Action = "ec2:DescribeNetworkInterfaces",
+        Effect = "Allow",
+        Resource = "*"
+      },
+      {
+        Action = "ec2:DeleteNetworkInterface",
+        Effect = "Allow",
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
+  role       = aws_iam_role.lambda_execution_role.name
+  policy_arn = aws_iam_policy.lambda_policy.arn
+}
+
+resource "aws_lambda_function" "health_check_lambda" {
+  function_name = "health_check_lambda"
+  role          = aws_iam_role.lambda_execution_role.arn
+  handler       = "index.handler"
+  runtime       = "nodejs18.x"
+
+  source_code_hash = filebase64sha256(data.archive_file.zip_index_file)
+  filename         = data.archive_file.zip_index_file
+
+  environment {
+    variables = {
+      TABLE_NAME      = "latestHealthCheck"
+      WEBHOOK_TABLE   = "discordWebhooks"
+    }
+  }
+
+  timeout = 30
+}
+
+resource "aws_cloudwatch_event_rule" "health_check_schedule" {
+  name                = "health_check_schedule"
+  schedule_expression = "rate(5 minutes)"
+}
+
+resource "aws_cloudwatch_event_target" "lambda_target" {
+  rule      = aws_cloudwatch_event_rule.health_check_schedule.name
+  target_id = "health_check_lambda"
+  arn       = aws_lambda_function.health_check_lambda.arn
+}
+
+resource "aws_lambda_permission" "allow_eventbridge" {
+  statement_id  = "AllowExecutionFromEventBridge"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.health_check_lambda.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.health_check_schedule.arn
+}
